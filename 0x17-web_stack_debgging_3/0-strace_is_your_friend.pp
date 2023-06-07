617494f2570cc51f6d3d2c32dfb6d7b9a2fb7b20
blackmoom2 <gideonlenge96@gmail.com>
2023-06-07 01:28:00 -0700

    0x17

--- 0x17-web_stack_debgging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debgging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Fixes a faulty wordpress site
-exes { 'fix-wordpress':
-  command => 'bash  -c "sed -1 s/class-wp-locale.phpp/class-wp-locale.php/ \
+exec { 'fix-wordpress':
+  command => 'bash -c "sed -i s/class-wp-locale.phpp/class-wp-locale.php/ \
 /var/www/html/wp-settings.php; service apache2 restart"',
-  path	=> '/usr/bin:/usr/sbin:/bin'
+  path    => '/usr/bin:/usr/sbin:/bin'
 }
